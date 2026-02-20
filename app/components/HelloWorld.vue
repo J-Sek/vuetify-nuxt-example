541bea69752860c45b8603dc9f4a2ecb87387d0e
J-Sek <J-Sek@users.noreply.github.com>
2026-02-20 18:37:11 +0100

    migrate HelloWorld.vue

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="h-full flex items-center" max-width="900">
+  <v-container class="fill-height d-flex align-center" max-width="900">
     <div>
       <v-img
         class="mb-4"
@@ -9,13 +9,13 @@
       />
 
       <div class="mb-8 text-center">
-        <div class="font-light -mb-1">Welcome to</div>
+        <div class="font-weight-light mb-n1">Welcome to</div>
         <h1 class="text-h1">Vuetify</h1>
       </div>
 
-      <div class="flex flex-wrap gap-3">
+      <div class="d-flex flex-wrap ga-3">
         <v-card
-          class="py-4 sm:pr-[120px] w-full dark:bg-primary-800 rounded-xl"
+          class="py-4 pr-sm-[120px] w-full bg-primary rounded-xl"
           color="surface-variant"
           image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
           variant="tonal"
@@ -25,17 +25,17 @@
           </template>
 
           <template #image>
-            <v-img class="hidden sm:block my-auto" height="120" position="top right" alt="Hero card supplemental image" />
+            <v-img class="d-none d-sm-block my-auto" height="120" position="top right" alt="Hero card supplemental image" />
           </template>
 
           <template #title>
-            <h2 class="text-h5 font-medium pt-1 translate-y-1">
+            <h2 class="text-h5 font-weight-medium pt-1 translate-y-[2px]">
               Get started
             </h2>
           </template>
 
           <template #subtitle>
-            <div class="text-subtitle-1 mt-2 leading-4">
+            <div class="text-subtitle-1 mt-2 line-height-[1]">
               Change this page by updating <v-code>{{ `<HelloWorld />` }}</v-code> in <v-code>components/HelloWorld.vue</v-code>.
             </div>
           </template>
@@ -46,8 +46,7 @@
           :append-icon="mdiArrowTopRight"
           class="
             rounded-xl
-            sm:flex-basis-[40%] flex-grow-1 h-[120px] flex items-center [&>.v-card-item]:w-full
-            dark:bg-linear-to-r dark:from-secondary dark:to-secondary/60
+            flex-basis-sm-[40%] flex-grow-[1] h-[120px] d-flex align-center [&>.v-card-item]:w-100
           "
           color="surface-variant"
           :href="link.href"
